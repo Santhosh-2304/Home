9d78b012b80ab3629988945b08ebba2f64e40d26
Santhosh-2304 <lionelsanthosh2004@gmail.com>
2025-09-25 13:39:13 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const LightSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  room: { type: String, required: false },
+  on: { type: Boolean, default: false }
+});
+
+const HomeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  lights: [LightSchema]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Home', HomeSchema);
